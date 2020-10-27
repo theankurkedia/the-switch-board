d4a3b706de33e0522c652afe6a2a9cbe1d65941d
Ankur Kedia <theankurkedia@gmail.com>
2020-10-27 23:33:03 +0530

    Added icons and updated script

--- background.js
+++ background.js
@@ -3,15 +3,18 @@ chrome.contextMenus.create({
     title: "Turn off the lights",
     contexts: ["all"]
 });
-
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (tab) {
-        var code = [
-            'var htmlTag = document.getElementsByTagName("html");',
-            'if (htmlTag && htmlTag.length) {' +
-            'htmlTag[0].style.filter = "invert(1) hue-rotate(180deg)";' +
-            '}'
+        let code = [
+            "var style = document.createElement('STYLE');",
+            "style.type = 'text/css';",
+            "style.appendChild(document.createTextNode('" +
+            "html { filter: invert(1) hue-rotate(180deg);}" +
+            "img { filter: invert(1) hue-rotate(-180deg);}" +
+            "iframe { filter: invert(1) hue-rotate(-180deg);}'));",
+            "document.getElementsByTagName('html')[0].appendChild(style);"
         ].join("\n");
-        chrome.tabs.executeScript(tab.id, { code: code });
+        on = false;
+        chrome.tabs.executeScript(tab.id, { code });
     }
 });
