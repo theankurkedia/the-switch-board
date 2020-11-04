d21c98e5c594da7f3f736f12005fc482f8186215
Ankur Kedia <theankurkedia@gmail.com>
2020-11-04 23:33:28 +0530

    Added mode toggle

--- background.js
+++ background.js
@@ -1,20 +1,30 @@
 chrome.contextMenus.create({
     id: "switch-board",
-    title: "Turn off the lights",
+    title: "Switch mode",
     contexts: ["all"]
 });
+
+function toggleMode(tab) {
+    let code = [
+        "var style = document.getElementById('the-switch-board-dark-mode-style');",
+        "if(style){",
+        "style.remove();",
+        "}",
+        "else{",
+        "style = document.createElement('STYLE');",
+        "style.setAttribute('id','the-switch-board-dark-mode-style')",
+        "style.type = 'text/css';",
+        "style.appendChild(document.createTextNode('" +
+        "html { filter: invert(1) hue-rotate(180deg);}" +
+        "img { filter: invert(1) hue-rotate(-180deg);}" +
+        "iframe { filter: invert(1) hue-rotate(-180deg);}'));",
+        "document.getElementsByTagName('html')[0].appendChild(style);",
+        "}"
+    ].join("\n");
+    chrome.tabs.executeScript(tab.id, { code });
+}
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (tab) {
-        let code = [
-            "var style = document.createElement('STYLE');",
-            "style.type = 'text/css';",
-            "style.appendChild(document.createTextNode('" +
-            "html { filter: invert(1) hue-rotate(180deg);}" +
-            "img { filter: invert(1) hue-rotate(-180deg);}" +
-            "iframe { filter: invert(1) hue-rotate(-180deg);}'));",
-            "document.getElementsByTagName('html')[0].appendChild(style);"
-        ].join("\n");
-        on = false;
-        chrome.tabs.executeScript(tab.id, { code });
+    if (tab && info.menuItemId === "switch-board") {
+        toggleMode(tab)
     }
 });
